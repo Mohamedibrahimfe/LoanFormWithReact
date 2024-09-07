69b9e7e7ac2e35fc9ea65e81e47c7bf2f1773800
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-09-08 02:25:34 +0300

    functionality finihed

--- src/Form.js
+++ src/Form.js
@@ -1,26 +1,76 @@
 import { useState } from "react";
 
+import Modal from "./Modal";
 export default function Form() {
+  const [form, setForm] = useState({
+    name: "",
+    phone: "",
+    age: "",
+    isEmployee: false,
+    values: "",
+    isButtonShown: false,
+  });
+
+  function hanldeSubmit(e) {
+    if (form.name !== "" && form.phone !== "" && form.age !== "") {
+      setForm({ ...form, isButtonShown: true });
+      alert("nio");
+    }
+  }
+  const inputsAreEmpty =
+    form.name === "" || form.phone === "" || form.age === "";
   return (
     <div className="form">
       <h1>Requesting a Loan </h1>
       <hr />
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Phone Number" />
-      <input type="number" placeholder="age" />
+      <input
+        value={form.name}
+        onChange={(event) => {
+          setForm({ ...form, name: event.target.value });
+        }}
+        type="text"
+        placeholder="Name"
+      />
+      <input
+        value={form.phone}
+        onChange={(e) => {
+          setForm({ ...form, phone: e.target.value });
+        }}
+        type="text"
+        placeholder="Phone Number"
+      />
+      <input
+        value={form.age}
+        onChange={(e) => setForm({ ...form, age: e.target.value })}
+        type="text"
+        placeholder="age"
+      />
       <label>R U an Employee</label>
       <div>
-        <input type="checkbox" />
+        <input
+          onChange={(e) => setForm({ ...form, isEmployee: e.target.checked })}
+          type="checkbox"
+        />
       </div>
       <label>How much your salary</label>
       <div>
-        <select>
-          <option value="1">from 1k to 3k</option>
-          <option value="2">from 3k to 5k</option>
-          <option value="3">from 5k to 10k</option>
+        <select
+          value={form.values}
+          onChange={(e) => setForm({ ...form, values: e.target.value })}
+        >
+          <option>from 1k to 3k</option>
+          <option>from 3k to 5k</option>
+          <option>from 5k to 10k</option>
         </select>
       </div>
-      <button>Submit</button>
+      <button
+        className={inputsAreEmpty ? "disabled" : ""}
+        disabled={inputsAreEmpty}
+        onClick={hanldeSubmit}
+      >
+        Submit
+      </button>
+      {/* <Modal showModal={showModal} /> */}
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,7 @@
+export default function Modal(showModal) {
+  return (
+    <div className="modal">
+      <h1>You have submitted your form</h1>
+    </div>
+  );
+}
